4ebe19cce8629c46fe56a59cb7689339fbc41fcf
Abraham Kuri <kurenn@icalialabs.com>
2015-11-25 11:19:05 -0600

    Updates municipality zip_code filter para name

--- app/models/municipality.rb
+++ app/models/municipality.rb
@@ -1,14 +1,14 @@
 class Municipality < ActiveRecord::Base
   belongs_to :state
 
-  scope :find_by_cp, lambda { |cp|
+  scope :find_by_zip_code, lambda { |cp|
     where("zip_code ILIKE ?", "%#{cp}%")
   }
 
   def self.search(params = {})
     municipalities = all
 
-    municipalities = municipalities.find_by_cp(params[:cp]) if params[:cp].present?
+    municipalities = municipalities.find_by_cp(params[:zip_code]) if params[:zip_code].present?
 
     municipalities
   end
