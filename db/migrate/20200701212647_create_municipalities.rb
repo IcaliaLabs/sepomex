ef0d7e68cf5452db1be65875fa08ec9a4f0d1eb5
Sandra González Díaz <58184612+sgdiaz0488@users.noreply.github.com>
2020-07-02 14:05:38 -0500

    Add models (#26)
    * Model ZipCode
    
    * Add model State
    
    * Add model Municipality
    
    * Add model City
    
    * Add AddUnaccentExtension
    
    * Generating migrations
    
    * Adding models

---
+++ db/migrate/20200701212647_create_municipalities.rb
@@ -0,0 +1,11 @@
+class CreateMunicipalities < ActiveRecord::Migration[6.0]
+  def change
+    create_table :municipalities do |t|
+      t.string :name, null: false
+      t.string :municipality_key, null: false
+      t.string :zip_code, null: false
+      t.integer :state_id
+    end
+    add_index(:municipalities, :state_id)
+  end
+end
