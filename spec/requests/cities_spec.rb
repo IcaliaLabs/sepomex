c3881bd51bed0ef6097c5a0efd4642ea067e720a
Abraham Kuri <kurenn@icalialabs.com>
2020-07-10 16:01:37 -0500

    Refactors city requests specs

--- spec/requests/cities_spec.rb
+++ spec/requests/cities_spec.rb
@@ -4,20 +4,23 @@ require 'rails_helper'
 
 RSpec.describe 'City requests' do
   describe 'GET/city' do
+    before do
+      get('/cities')
+    end
+
     it 'is a valid route' do
-      get('/city')
       expect(response).to have_http_status(:success)
     end
 
     it 'return results' do
-      get('/city')
       results = JSON.parse(response.body)
+
       expect(results).to include('cities')
     end
 
     it 'has pagination' do
-      get('/city')
       results = JSON.parse(response.body)
+
       expect(results).to include('meta')
       expect(results['meta']).to include('pagination')
     end
