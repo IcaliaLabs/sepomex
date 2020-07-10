0ef34d4219caa25e27d9bebe044323e5a03ecc89
Abraham Kuri <kurenn@icalialabs.com>
2020-07-10 16:01:06 -0500

    Refactors the states resources with specs

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -1,17 +1,18 @@
 # frozen_string_literal: true
 
-class StateController < ApplicationController
-  # GET '/state'
+class StatesController < ApplicationController
+
+  # GET '/states'
   def index
     paginate State.unscoped, per_page: 15
   end
 
-  # GET '/state/:id'
+  # GET '/states/:id'
   def show
     render json: State.find(params[:id])
   end
 
-  # GET '/state/:id/municipalities'
+  # GET '/states/:id/municipalities'
   def municipalities
     state = State.find(params[:id])
     render json: state.municipalities.order(:id)
--- spec/requests/states_spec.rb
+++ spec/requests/states_spec.rb
@@ -4,20 +4,23 @@ require 'rails_helper'
 
 RSpec.describe 'State requests' do
   describe 'GET/state' do
+    before do
+      get('/states')
+    end
+
     it 'is a valid route' do
-      get('/state')
       expect(response).to have_http_status(:success)
     end
 
     it 'return results' do
-      get('/state')
       results = JSON.parse(response.body)
+
       expect(results).to include('states')
     end
 
     it 'has pagination' do
-      get('/state')
       results = JSON.parse(response.body)
+
       expect(results).to include('meta')
       expect(results['meta']).to include('pagination')
     end
