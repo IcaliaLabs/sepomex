11c5158361f26738e7081dade95b4850abd6a1c5
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2022-02-17 06:36:42 +0000

    Use SQL & CTE's to start the  CSV import

--- app/services/load_csv_to_database.rb
+++ app/services/load_csv_to_database.rb
@@ -7,7 +7,7 @@ require 'csv'
 # Loads the CSV into the database
 class LoadCsvToDatabase
   include Performable
-  
+
   FILE_PATH = 'lib/sepomex_db.csv'
 
   def initialize
@@ -27,60 +27,114 @@ class LoadCsvToDatabase
 
   protected
 
-  def find_or_create_zipcode(row)
-    notify_load_progress "Processing #{row[0]}. #{row[3]}, #{row[4]}..."
-    
-    ZipCode.create_with(
-      d_asenta: row[1],         # Nombre asentamiento
-      d_tipo_asenta: row[2],    # Tipo de asentamiento (Catálogo SEPOMEX)
-      d_mnpio: row[3],          # Nombre Municipio (INEGI, Marzo 2013)
-      d_estado: row[4],         # Nombre Entidad (INEGI, Marzo 2013)
-      d_ciudad: row[5],         # Nombre Ciudad (Catálogo SEPOMEX)
-      d_cp: row[6],             # Código Postal de la Administración Postal que reparte al asentamiento
-      c_estado: row[7],         # Clave Entidad (INEGI, Marzo 2013)
-      c_oficina: row[8],        # Código Postal de la Administración Postal que reparte al asentamiento
-      c_cp: row[9],             # Campo Vacio
-      c_tipo_asenta: row[10],   # Clave Tipo de asentamiento (Catálogo SEPOMEX)
-      c_mnpio: row[11],         # Clave Municipio (INEGI, Marzo 2013)
-      d_zona: row[13],          # Zona en la que se ubica el asentamiento (Urbano/Rural)
-      c_cve_ciudad: row[14]     # Clave Ciudad (Catálogo SEPOMEX)
-    ).find_or_create_by(
-      d_codigo: row[0],         # Código Postal asentamiento
-      id_asenta_cpcons: row[12] # Identificador único del asentamiento (nivel municipal)
-    )
+  def values
+    return @values if @values
+
+    @values = []
+
+    CSV.foreach(FILE_PATH, quote_char: "\x00", col_sep: '|', encoding: 'UTF-8') do |row|
+      column_values = row.map { |value| value.blank? ? 'NULL' : "'#{value}'" }
+      @values << "(#{column_values.join(', ')})"
+    end
+
+    @values = "VALUES #{values.join(', ')}"
   end
 
+  def import_cte
+    <<~SQL.squish
+      WITH "input_data" AS (
+        SELECT * FROM (
+          #{values}
+        ) AS "input_data" (
+          "d_codigo", "d_asenta", "d_tipo_asenta", "d_mnpio", "d_estado", "d_ciudad",
+          "d_cp", "c_estado", "c_oficina", "c_cp", "c_tipo_asenta", "c_mnpio",
+          "id_asenta_cpcons", "d_zona", "c_cve_ciudad"
+        )
+      ), "updates" AS (
+        SELECT
+          "zip_codes"."id",
+          "input_data".*,
+          COALESCE("zip_codes"."created_at", NOW()) AS "created_at",
+          NOW() AS "updated_at"
+        FROM
+          "input_data"
+          LEFT JOIN "zip_codes" ON
+            "input_data"."d_codigo" = "zip_codes"."d_codigo"
+            AND "input_data"."id_asenta_cpcons" = "zip_codes"."id_asenta_cpcons"
+      )
+    SQL
+  end
+
+  def insert_sql
+    <<~SQL.squish
+      #{import_cte}
+      INSERT INTO "zip_codes" (
+        "d_codigo", "d_asenta", "d_tipo_asenta", "d_mnpio", "d_estado", "d_ciudad",
+        "d_cp", "c_estado", "c_oficina", "c_cp", "c_tipo_asenta", "c_mnpio",
+        "id_asenta_cpcons", "d_zona", "c_cve_ciudad", "created_at", "updated_at"
+      ) SELECT
+        "d_codigo", "d_asenta", "d_tipo_asenta", "d_mnpio", "d_estado", "d_ciudad",
+        "d_cp", "c_estado", "c_oficina", "c_cp", "c_tipo_asenta", "c_mnpio",
+        "id_asenta_cpcons", "d_zona", "c_cve_ciudad", "created_at", "updated_at"
+      FROM "updates" WHERE "id" IS NULL
+    SQL
+  end
+
+  def update_sql
+    <<~SQL.squish
+      #{import_cte}
+      UPDATE "zip_codes" SET
+        "d_codigo"         = "updates"."d_codigo",
+        "d_asenta"         = "updates"."d_asenta",
+        "d_tipo_asenta"    = "updates"."d_tipo_asenta",
+        "d_mnpio"          = "updates"."d_mnpio",
+        "d_estado"         = "updates"."d_estado",
+        "d_ciudad"         = "updates"."d_ciudad",
+        "d_cp"             = "updates"."d_cp",
+        "c_estado"         = "updates"."c_estado",
+        "c_oficina"        = "updates"."c_oficina",
+        "c_cp"             = "updates"."c_cp",
+        "c_tipo_asenta"    = "updates"."c_tipo_asenta",
+        "c_mnpio"          = "updates"."c_mnpio",
+        "id_asenta_cpcons" = "updates"."id_asenta_cpcons",
+        "d_zona"           = "updates"."d_zona",
+        "c_cve_ciudad"     = "updates"."c_cve_ciudad",
+        "updated_at"       = "updates"."updated_at"
+      FROM "updates" WHERE "zip_codes"."id" = "updates"."id"
+    SQL
+  end
+
+  delegate :connection, to: ActiveRecord::Base,   prefix: :database
+  delegate :execute,    to: :database_connection, prefix: :database
+
   def update_zip_code_table
-    notify_load_progress 'Filling the table zip_codes'
-  
-    CSV.foreach(FILE_PATH, col_sep: '|', encoding: 'UTF-8') do |row|
-      find_or_create_zipcode(row)
-    end
-  
-    notify_load_progress 'Zipcode filling finished'
+    notify_load_progress 'Updating the "zip_codes" table...'
+    database_execute insert_sql
+    database_execute update_sql
+    notify_load_progress '..."zip_codes" updating finished'
   end
 
   def update_states_table
     notify_load_progress 'Creating states...'
-  
+
     state_names = ZipCode.pluck(:d_estado).uniq
     state_names.each do |state_name|
       cities_count = ZipCode.where(d_estado: state_name).pluck(:d_mnpio).uniq.count
-  
+
       notify_load_progress "Creating #{state_name}."
       State.find_or_create_by(name: state_name, cities_count: cities_count)
     end
     notify_load_progress 'Done!'
   end
-  
+
   def update_municipalities_table
     states = State.all
     states.each do |state|
       municipalities = ZipCode.where(d_estado: state.name)
-  
+
       municipalities.each do |municipality|
         next if Municipality.find_by_name(municipality.d_mnpio)
-  
+
         notify_load_progress "Creating #{municipality.d_mnpio}."
         state.municipalities.find_or_create_by(name: municipality.d_mnpio,
                                                municipality_key: municipality.c_mnpio,
@@ -89,21 +143,21 @@ class LoadCsvToDatabase
     end
     notify_load_progress 'Done!'
   end
-  
+
   def update_cities_table
     notify_load_progress 'Creating cities...'
-  
+
     states = State.all
     states.each do |state|
       zip_codes_by_state = ZipCode.where(d_estado: state.name)
-  
+
       zip_codes_by_state.each do |zip_code|
         next if City.find_by_name(zip_code.d_ciudad)
-  
+
         city_name = 'N/A'
-  
+
         city_name = zip_code.d_ciudad if zip_code.d_ciudad.present?
-  
+
         notify_load_progress "Creating #{city_name}."
         state.cities.find_or_create_by(name: city_name)
       end
