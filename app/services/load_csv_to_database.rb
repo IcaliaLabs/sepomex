6b1c83b765bb27491778a5a6947f648a73f736ae
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2022-08-13 05:15:46 +0000

    Refactored LoadCsvToDatabase#update_municipalities_table

--- app/services/load_csv_to_database.rb
+++ app/services/load_csv_to_database.rb
@@ -157,20 +157,61 @@ class LoadCsvToDatabase
     notify_load_progress 'Done!'
   end
 
+  def municipality_import_cte
+    <<~SQL.squish
+      WITH "input_data_source" AS (
+        SELECT DISTINCT ON ("c_estado", "c_mnpio")
+          "d_mnpio" AS "name",
+          "c_mnpio" AS "municipality_key",
+          "d_cp" AS "zip_code",
+          "d_estado" AS "state_name"
+        FROM "zip_codes" ORDER BY "c_estado", "c_mnpio" ASC, "d_cp" DESC
+      ), "input_data" AS (
+        SELECT
+          "input_data_source"."name",
+          "input_data_source"."municipality_key",
+          "input_data_source"."zip_code",
+          "states"."id" AS "state_id"
+        FROM
+          "input_data_source"
+          INNER JOIN "states" ON "input_data_source"."state_name" = "states"."name"
+      ), "updates" AS (
+        SELECT "m"."id", "i".*
+        FROM "input_data" AS "i" LEFT JOIN "municipalities" AS "m" ON
+          "i"."name" = "m"."name"
+          AND "i"."municipality_key" = "m"."municipality_key"
+          AND "i"."state_id" = "m"."state_id"
+      )
+    SQL
+  end
+
+  def update_existing_municipalities
+    database_execute <<~SQL.squish
+      #{municipality_import_cte}
+      UPDATE "municipalities" SET
+        "name" = "updates"."name",
+        "municipality_key" = "updates"."municipality_key",
+        "zip_code" = "updates"."zip_code",
+        "state_id" = "updates"."state_id"
+      FROM "updates" WHERE "municipalities"."id" = "updates"."id"
+    SQL
+  end
+
+  def insert_missing_municipalities
+    database_execute <<~SQL.squish
+      #{municipality_import_cte}
+      INSERT INTO "municipalities" ("name", "municipality_key", "zip_code", "state_id")
+      SELECT "name", "municipality_key", "zip_code", "state_id"
+      FROM "updates"
+      WHERE "id" IS NULL
+      ORDER BY "state_id", "municipality_key" ASC
+    SQL
+  end
+
   def update_municipalities_table
-    states = State.all
-    states.each do |state|
-      municipalities = ZipCode.where(d_estado: state.name)
-
-      municipalities.each do |municipality|
-        next if Municipality.find_by_name(municipality.d_mnpio)
-
-        notify_load_progress "Creating #{municipality.d_mnpio}."
-        state.municipalities.find_or_create_by(name: municipality.d_mnpio,
-                                               municipality_key: municipality.c_mnpio,
-                                               zip_code: municipality.d_cp)
-      end
-    end
+    notify_load_progress 'Updating municipalities table...'
+    update_existing_municipalities
+    insert_missing_municipalities
     notify_load_progress 'Done!'
   end
 
