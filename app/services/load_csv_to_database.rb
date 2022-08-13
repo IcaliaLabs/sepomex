c24fbfc96ffbbcf4414ee34e4441e2a7695b67f1
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2022-08-13 06:29:12 +0000

    Refactored LoadCsvToDatabase#update_cities_table

--- app/services/load_csv_to_database.rb
+++ app/services/load_csv_to_database.rb
@@ -215,24 +215,50 @@ class LoadCsvToDatabase
     notify_load_progress 'Done!'
   end
 
+  def city_import_cte
+    <<~SQL.squish
+      WITH "input_source" AS (
+        SELECT DISTINCT ON ("c_estado", "c_cve_ciudad")
+          "d_ciudad" AS "name", "d_estado" AS "state_name"
+        FROM "zip_codes"
+        WHERE "c_cve_ciudad" IS NOT NULL
+        ORDER BY "c_estado", "c_cve_ciudad" ASC
+      ), "input_data" AS (
+        SELECT DISTINCT "i"."name", "s"."id" AS "state_id"
+        FROM "input_source" AS "i" INNER JOIN "states" AS "s" ON
+          "i"."state_name" = "s"."name"
+      ), "updates" AS (
+        SELECT "c"."id", "i".*
+        FROM "input_data" AS "i" LEFT JOIN "cities" AS "c" ON
+          "i"."name" = "c"."name" AND "i"."state_id" = "c"."state_id"
+      )
+    SQL
+  end
+
+  def update_existing_cities
+    database_execute <<~SQL.squish
+      #{city_import_cte}
+      UPDATE "cities" SET
+        "name" = "updates"."name", "state_id" = "updates"."state_id"
+      FROM "updates" WHERE "cities"."id" = "updates"."id"
+    SQL
+  end
+
+  def insert_missing_cities
+    database_execute <<~SQL.squish
+      #{city_import_cte}
+      INSERT INTO "cities" ("name", "state_id")
+      SELECT "name", "state_id"
+      FROM "updates"
+      WHERE "id" IS NULL
+      ORDER BY "state_id", "name" ASC
+    SQL
+  end
+
   def update_cities_table
     notify_load_progress 'Creating cities...'
-
-    states = State.all
-    states.each do |state|
-      zip_codes_by_state = ZipCode.where(d_estado: state.name)
-
-      zip_codes_by_state.each do |zip_code|
-        next if City.find_by_name(zip_code.d_ciudad)
-
-        city_name = 'N/A'
-
-        city_name = zip_code.d_ciudad if zip_code.d_ciudad.present?
-
-        notify_load_progress "Creating #{city_name}."
-        state.cities.find_or_create_by(name: city_name)
-      end
-    end
+    update_existing_cities
+    insert_missing_cities
     notify_load_progress 'Done!'
   end
 
