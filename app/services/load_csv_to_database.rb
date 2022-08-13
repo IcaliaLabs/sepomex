26c4239a585bdce860997fd83f4cd17d4c1d3b50
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2022-08-13 07:11:38 +0000

    Extracted the zip codes table update to a child service

--- app/services/load_csv_to_database.rb
+++ app/services/load_csv_to_database.rb
@@ -15,7 +15,8 @@ class LoadCsvToDatabase
   end
 
   def perform!
-    update_zip_code_table
+    read_csv_data
+    UpdateZipCodesTable.perform! self
     UpdateStatesTable.perform! self
     UpdateMunicipalitiesTable.perform! self
     UpdateCitiesTable.perform! self
@@ -40,79 +41,14 @@ class LoadCsvToDatabase
     @values = "VALUES #{values.join(', ')}"
   end
 
-  def import_cte
-    <<~SQL.squish
-      WITH "input_data" AS (
-        SELECT * FROM (
-          #{values}
-        ) AS "input_data" (
-          "d_codigo", "d_asenta", "d_tipo_asenta", "d_mnpio", "d_estado", "d_ciudad",
-          "d_cp", "c_estado", "c_oficina", "c_cp", "c_tipo_asenta", "c_mnpio",
-          "id_asenta_cpcons", "d_zona", "c_cve_ciudad"
-        )
-      ), "updates" AS (
-        SELECT
-          "zip_codes"."id",
-          "input_data".*,
-          COALESCE("zip_codes"."created_at", NOW()) AS "created_at",
-          NOW() AS "updated_at"
-        FROM
-          "input_data"
-          LEFT JOIN "zip_codes" ON
-            "input_data"."d_codigo" = "zip_codes"."d_codigo"
-            AND "input_data"."id_asenta_cpcons" = "zip_codes"."id_asenta_cpcons"
-      )
-    SQL
-  end
-
-  def insert_zip_codes
-    database_execute <<~SQL.squish
-      #{import_cte}
-      INSERT INTO "zip_codes" (
-        "d_codigo", "d_asenta", "d_tipo_asenta", "d_mnpio", "d_estado", "d_ciudad",
-        "d_cp", "c_estado", "c_oficina", "c_cp", "c_tipo_asenta", "c_mnpio",
-        "id_asenta_cpcons", "d_zona", "c_cve_ciudad", "created_at", "updated_at"
-      ) SELECT
-        "d_codigo", "d_asenta", "d_tipo_asenta", "d_mnpio", "d_estado", "d_ciudad",
-        "d_cp", "c_estado", "c_oficina", "c_cp", "c_tipo_asenta", "c_mnpio",
-        "id_asenta_cpcons", "d_zona", "c_cve_ciudad", "created_at", "updated_at"
-      FROM "updates" WHERE "id" IS NULL
-    SQL
-  end
-
-  def update_zip_codes
-    database_execute <<~SQL.squish
-      #{import_cte}
-      UPDATE "zip_codes" SET
-        "d_codigo"         = "updates"."d_codigo",
-        "d_asenta"         = "updates"."d_asenta",
-        "d_tipo_asenta"    = "updates"."d_tipo_asenta",
-        "d_mnpio"          = "updates"."d_mnpio",
-        "d_estado"         = "updates"."d_estado",
-        "d_ciudad"         = "updates"."d_ciudad",
-        "d_cp"             = "updates"."d_cp",
-        "c_estado"         = "updates"."c_estado",
-        "c_oficina"        = "updates"."c_oficina",
-        "c_cp"             = "updates"."c_cp",
-        "c_tipo_asenta"    = "updates"."c_tipo_asenta",
-        "c_mnpio"          = "updates"."c_mnpio",
-        "id_asenta_cpcons" = "updates"."id_asenta_cpcons",
-        "d_zona"           = "updates"."d_zona",
-        "c_cve_ciudad"     = "updates"."c_cve_ciudad",
-        "updated_at"       = "updates"."updated_at"
-      FROM "updates" WHERE "zip_codes"."id" = "updates"."id"
-    SQL
+  def read_csv_data
+    notify_load_progress 'Reading CSV data...'
+    values
+    notify_load_progress 'Done!'
   end
 
   delegate :connection, to: ActiveRecord::Base,   prefix: :database
   delegate :execute,    to: :database_connection, prefix: :database
-
-  def update_zip_code_table
-    notify_load_progress 'Updating the "zip_codes" table...'
-    insert_zip_codes
-    update_zip_codes
-    notify_load_progress '..."zip_codes" updating finished'
-  end
 
   def notify_load_progress(*args)
     Rails.logger.debug { "Load progress: #{args.inspect}" }
---
+++ app/services/load_csv_to_database/update_zip_codes_table.rb
@@ -0,0 +1,87 @@
+# frozen_string_literal: true
+
+class LoadCsvToDatabase
+  #= LoadCsvToDatabase::UpdateZipCodesTable
+  #
+  # Updates the zip codes table using the data loaded from the CSV file
+  # NOTE: In reality, the zip codes table contains a list of settlements, not
+  # zip codes.
+  class UpdateZipCodesTable < BaseUpdateTable
+    def perform!
+      notify_load_progress 'Updating zip codes table...'
+      update_existing_records
+      add_missing_records
+      notify_load_progress 'Done!'
+    end
+
+    protected
+
+    def values
+      data_loader.send :values
+    end
+
+    def zip_codes_import_cte
+      <<~SQL.squish
+        WITH "input_data" AS (
+          SELECT * FROM (
+            #{values}
+          ) AS "input_data" (
+            "d_codigo", "d_asenta", "d_tipo_asenta", "d_mnpio", "d_estado", "d_ciudad",
+            "d_cp", "c_estado", "c_oficina", "c_cp", "c_tipo_asenta", "c_mnpio",
+            "id_asenta_cpcons", "d_zona", "c_cve_ciudad"
+          )
+        ), "updates" AS (
+          SELECT
+            "zip_codes"."id",
+            "input_data".*,
+            COALESCE("zip_codes"."created_at", NOW()) AS "created_at",
+            NOW() AS "updated_at"
+          FROM
+            "input_data"
+            LEFT JOIN "zip_codes" ON
+              "input_data"."d_codigo" = "zip_codes"."d_codigo"
+              AND "input_data"."id_asenta_cpcons" = "zip_codes"."id_asenta_cpcons"
+        )
+      SQL
+    end
+
+    def add_missing_records
+      database_execute <<~SQL.squish
+        #{zip_codes_import_cte}
+        INSERT INTO "zip_codes" (
+          "d_codigo", "d_asenta", "d_tipo_asenta", "d_mnpio", "d_estado", "d_ciudad",
+          "d_cp", "c_estado", "c_oficina", "c_cp", "c_tipo_asenta", "c_mnpio",
+          "id_asenta_cpcons", "d_zona", "c_cve_ciudad", "created_at", "updated_at"
+        ) SELECT
+          "d_codigo", "d_asenta", "d_tipo_asenta", "d_mnpio", "d_estado", "d_ciudad",
+          "d_cp", "c_estado", "c_oficina", "c_cp", "c_tipo_asenta", "c_mnpio",
+          "id_asenta_cpcons", "d_zona", "c_cve_ciudad", "created_at", "updated_at"
+        FROM "updates" WHERE "id" IS NULL
+      SQL
+    end
+
+    def update_existing_records
+      database_execute <<~SQL.squish
+        #{zip_codes_import_cte}
+        UPDATE "zip_codes" SET
+          "d_codigo"         = "updates"."d_codigo",
+          "d_asenta"         = "updates"."d_asenta",
+          "d_tipo_asenta"    = "updates"."d_tipo_asenta",
+          "d_mnpio"          = "updates"."d_mnpio",
+          "d_estado"         = "updates"."d_estado",
+          "d_ciudad"         = "updates"."d_ciudad",
+          "d_cp"             = "updates"."d_cp",
+          "c_estado"         = "updates"."c_estado",
+          "c_oficina"        = "updates"."c_oficina",
+          "c_cp"             = "updates"."c_cp",
+          "c_tipo_asenta"    = "updates"."c_tipo_asenta",
+          "c_mnpio"          = "updates"."c_mnpio",
+          "id_asenta_cpcons" = "updates"."id_asenta_cpcons",
+          "d_zona"           = "updates"."d_zona",
+          "c_cve_ciudad"     = "updates"."c_cve_ciudad",
+          "updated_at"       = "updates"."updated_at"
+        FROM "updates" WHERE "zip_codes"."id" = "updates"."id"
+      SQL
+    end
+  end
+end
