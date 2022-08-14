a8d694823c069801a2dd91315f253dbd79348a7a
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2022-08-14 02:30:16 +0000

    Refactored UpdateZipCodesTable to use a temp table to dump CSV data

--- app/services/load_csv_to_database/update_zip_codes_table.rb
+++ app/services/load_csv_to_database/update_zip_codes_table.rb
@@ -7,15 +7,39 @@ class LoadCsvToDatabase
   # NOTE: In reality, the zip codes table contains a list of settlements, not
   # zip codes.
   class UpdateZipCodesTable < BaseUpdateTable
+    attr_reader :load_timestamp, :tmp_table_name
+
     def perform!
       notify_load_progress 'Updating zip codes table...'
+      dump_data_into_temporary_table
       update_existing_records
       add_missing_records
       delete_records_no_longer_present_on_input_data
+      drop_temporary_table
       notify_load_progress 'Done!'
     end
 
     protected
+
+    CSV_COLUMNS = %i[
+      d_codigo d_asenta d_tipo_asenta d_mnpio d_estado d_ciudad d_cp c_estado
+      c_oficina c_cp c_tipo_asenta c_mnpio id_asenta_cpcons d_zona c_cve_ciudad
+    ].freeze
+
+    def dump_data_into_temporary_table
+      @load_timestamp = Time.current.utc
+      @tmp_table_name = "csv_data_#{load_timestamp.strftime('%Y%m%d%H%M%S')}"
+
+      cols = CSV_COLUMNS.map { |c| "\"#{c}\" varchar" }.join ', '
+      database_execute "CREATE TEMPORARY TABLE \"#{tmp_table_name}\" (#{cols})"
+
+      cols = CSV_COLUMNS.map { |c| "\"#{c}\"" }.join ', '
+      database_execute "INSERT INTO \"#{tmp_table_name}\" (#{cols}) #{values}"
+    end
+
+    def drop_temporary_table
+      database_execute "DROP TABLE \"#{tmp_table_name}\""
+    end
 
     def values
       data_loader.send :values
@@ -24,27 +48,19 @@ class LoadCsvToDatabase
     def zip_codes_import_cte
       current_time_cast = "CAST('#{Time.current.utc.to_s(:db)}' AS TIMESTAMP)"
       <<~SQL.squish
-        WITH "input_data" AS (
-          SELECT * FROM (
-            #{values}
-          ) AS "input_data" (
-            "d_codigo", "d_asenta", "d_tipo_asenta", "d_mnpio", "d_estado", "d_ciudad",
-            "d_cp", "c_estado", "c_oficina", "c_cp", "c_tipo_asenta", "c_mnpio",
-            "id_asenta_cpcons", "d_zona", "c_cve_ciudad"
-          )
-        ), "updates" AS (
+        WITH "updates" AS (
           SELECT
             "z"."id",
             "i".*,
             COALESCE("z"."created_at", #{current_time_cast}) AS "created_at",
             #{current_time_cast} AS "updated_at"
-          FROM "input_data" AS "i" LEFT JOIN "zip_codes" AS "z" ON
+          FROM "#{tmp_table_name}" AS "i" LEFT JOIN "zip_codes" AS "z" ON
             "i"."c_estado" = "z"."c_estado"
             AND "i"."c_mnpio" = "z"."c_mnpio"
             AND "i"."id_asenta_cpcons" = "z"."id_asenta_cpcons"
         ), "deletes" AS (
           SELECT DISTINCT "z"."id"
-          FROM "zip_codes" AS "z" LEFT JOIN "input_data" AS "i" ON
+          FROM "zip_codes" AS "z" LEFT JOIN "#{tmp_table_name}" AS "i" ON
             "z"."c_estado" = "i"."c_estado"
             AND "z"."c_mnpio" = "i"."c_mnpio"
             AND "z"."id_asenta_cpcons" = "i"."id_asenta_cpcons"
