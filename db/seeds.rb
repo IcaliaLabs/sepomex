26a5bd47be1b8ed848492a260eade649bf98b653
Alan Mendicutti <alan.mendicuttimldn@gmail.com>
2023-08-24 21:18:20 +0000

    Seeding database only if production and if is empty

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+if Rails.env.production? && ENV['DEPLOY_NAME'] == 'production'
+  Municipality.count.zero? && (bundle exec rake 'data:load')
+end
